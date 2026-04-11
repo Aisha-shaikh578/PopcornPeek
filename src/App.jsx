e6b86dffd9f144bbb127f06b048199d18caf25e3
Aisha-shaikh578 <itsaishashaikh05@gmail.com>
2026-04-11 14:57:29 +0530

    Improving UI and displaying data

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ const App = () => {
   }
   
   return (
-    <div className='h-full p-5 md:px-14 lg:px-24 xl:px-35 2xl:px-58 bg-gray-300 w-full'>
+    <div className='h-full md:h-screen p-5 md:px-14 lg:px-24 xl:px-35 2xl:px-58 bg-gray-300 w-full'>
      <Navbar handleInput={handleInput} handleSearch={handleSearch}/>
      <Category />
-     <div className='flex flex-col md:flex-row gap-4 mt-6 justify-center items-center'>
+     <div className='flex flex-wrap gap-5 mt-6 justify-center items-center'>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {movies.map((movie,idx) => {
--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 const MovieCard = ({ movie }) => {
 
   return (
-    <div className='w-40 h-50 bg-gray-200 p-3 rounded-xl flex flex-col gap-3'>
-      <h2>{movie.Title}</h2>
-      <h3>{movie.Year}</h3>
-        <img src={movie.Poster} alt={movie.Title} className='h-22 md:h-15'/>
+    <div className='w-40 h-50 bg-gray-200 p-2 rounded-xl flex flex-col gap-2 relative'>
+      <div>
+        <h2 className='text-nowrap overflow-hidden text-ellipsis font-bold text-xl'>
+          {movie.Title}
+        </h2>
+        <h3>{movie.Year}</h3>
+      </div>
+      <div>
+        <img src={movie.Poster} alt={movie.Title} className='h-30 object-fill rounded absolute bottom-2.5 w-36'/>
+      </div>
     </div>
   )
 }
